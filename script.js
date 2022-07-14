b1d7d2ff5c4c3466e4f703e3ef0ef704f7705d1b
Edward Lee <edwardneo@berkeley.edu>
2022-07-14 03:43:48 -0700

    Added clear button and restructured code

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const map = L.map('testMap').setView([37.8757, -122.2593], 17);
-let range = {};
-const points = [];
-const datatypes = [];
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+const layerControl = L.control.layers().addTo(map);
+const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
+layerControl.addBaseLayer(osm, "OpenStreetMap");
+
+let datatypes = []; // [datatype, ...]
+let uploads = {}; // {filename: {points: [{data, marker}, ...], overlay, viewMarkers, datatypes}}
+let extrema = {}; // {datatype: [min, max]}
+let view = L.featureGroup();
 
 $("#render-data").click(() => {
     $('#data-input').parse({
@@ -25,72 +28,181 @@ $("#render-data").click(() => {
 	});
 });
 
-const plot = results => {
-    // Clean and parse data
-    const data = results.data.filter(dataPoint => parseFloat(dataPoint["Latitude"]) && parseFloat(dataPoint["Longitude"]));
-    datatypes.push(...results.meta.fields.filter(datatype => datatype !== "Latitude" && datatype !== "Longitude" && !datatypes.includes(datatype)));
-    range = datatypes.map(datatype => { // Sorts data by type
-        const dataByType = data.map(dataPoint => dataPoint[datatype]);
-        return [datatype, range[datatype] ? dataByType.concat(range[datatype]) : dataByType];
+$("#clear-data").click(() => {
+    const targetFn = $('#overlays option:selected').val();
+
+    if (targetFn === "All" || $("#overlays").children().length === 2) {
+        datatypes = [];
+        extrema = {};
+        view = L.featureGroup();
+        
+        Object.values(uploads).reduce((overlays, upload) => overlays.concat([upload.overlay]), [])
+            .forEach(overlay => {
+                overlay.removeFrom(map);
+                layerControl.removeLayer(overlay);
+        });
+
+        uploads = {};
+
+        $("#sensors").empty();
+
+        $("#overlays").empty();
+        $("#overlays").append($(`<option value="All">All</option>`));
+    } else {
+        const oldDatatypes = Object.keys(uploads)
+            .reduce((prevDatatypes, filename) => filename === targetFn ? prevDatatypes : prevDatatypes.concat(uploads[filename].datatypes), []);
+        const newDatatypes = uploads[targetFn].datatypes.filter(datatype => !oldDatatypes.includes(datatype));
+
+        datatypes = datatypes.filter(datatype => !newDatatypes.includes(datatype));
+
+        uploads[targetFn].overlay.removeFrom(map);
+        layerControl.removeLayer(uploads[targetFn].overlay);
+        uploads[targetFn].viewMarkers.forEach(marker => view.removeLayer(marker));
+
+        delete uploads[targetFn];
+        
+        extrema = findExtrema();
+
+        resetDesc();
+
+        map.flyToBounds(view.getBounds());
+
+        resetRadio(getDisplayedData());
+
+        $('#overlays option:selected').remove();
+    }
+});
+
+const plot = (results, file) => {
+    // Consolidate data
+    const data = results.data.filter(datapoint => parseFloat(datapoint["Latitude"]) && parseFloat(datapoint["Longitude"]));
+    const fileDatatypes = results.meta.fields.filter(datatype => datatype !== "Latitude" && datatype !== "Longitude");
+    const newDatatypes = !fileDatatypes.every(datatype => datatypes.includes(datatype));
+    datatypes.push(...fileDatatypes.filter(datatype => !datatypes.includes(datatype)));
+    const overlay = L.featureGroup();
+    uploads[file.name] = {
+        points: [],
+        overlay: overlay,
+        viewMarkers: [],
+        datatypes: fileDatatypes,
+    };
+
+    // Find datatype to display
+    const displayedData = getDisplayedData();
+
+    // Find extrema
+    extrema = {...extrema, ...fileDatatypes.map(datatype => { // Sorts data by type
+        const dataByType = data.map(datapoint => datapoint[datatype]);
+        return [datatype, extrema[datatype] ? dataByType.concat(extrema[datatype]) : dataByType];
     }).reduce((rangeObj, dataByType) => { // Finds extrema for each datatype
         rangeObj[dataByType[0]] = [Math.min(...dataByType[1]), Math.max(...dataByType[1])];
         return rangeObj;
-    }, {});
+    }, {})};
 
-    // Set display parameters
-    const latAvg = data.reduce((prev, dataPoint) => prev + parseFloat(dataPoint["Latitude"]), 0) / data.length;
-    const lonAvg = data.reduce((prev, dataPoint) => prev + parseFloat(dataPoint["Longitude"]), 0) / data.length;
-    map.setView([latAvg, lonAvg], 17);
-    resetColors(datatypes[0]);
+    // Plot and store points
+    uploads[file.name].points = data.map(datapoint => {
+        const lat = parseFloat(datapoint["Latitude"]);
+        const lon = parseFloat(datapoint["Longitude"]);
 
-    const displayedData = $("#sensors").children().length > 0 ? $('input[name="sensor"]:checked').val() : datatypes[0];
+        const viewMarker = L.marker([lat, lon]);
+        const visualMarker = L.circle([lat, lon], {
+            fillOpacity: 0.5,
+            radius: 5,
+        }).bindPopup("");
 
-    // Plot points
-    points.push(...data.map(dataPoint => {
-        const lat = parseFloat(dataPoint["Latitude"]);
-        const lon = parseFloat(dataPoint["Longitude"]);
-        const description = datatypes.reduce((descStr, datatype) => descStr += datatype + ": " + dataPoint[datatype] + "<br>", "");
-        const pointColor = rainbow(normalize(parseFloat(dataPoint[displayedData]), ...range[displayedData]));
+        viewMarker.addTo(view);
+        visualMarker.addTo(overlay);
 
-        return [
-            L.circle([lat, lon], {
-                color: pointColor,
-                fillOpacity: 0.5,
-                radius: 5
-            }).addTo(map)
-            .bindPopup(description),
-            dataPoint,
-        ];
-    }));
+        uploads[file.name].viewMarkers.push(viewMarker);
+
+        return {
+            data: datapoint,
+            marker: visualMarker,
+        };
+    });
+
+    // Update colors and descriptions
+    resetColors(displayedData);
+    resetDesc();
+
+    // Add overlay to map
+    overlay.addTo(map);
+    layerControl.addOverlay(overlay, file.name);
+
+    // Set view
+    $("#sensors").children() > 0 ? map.flyToBounds(view.getBounds()) : map.fitBounds(view.getBounds());
 
     // Create radio buttons
+    resetRadio(displayedData);
+
+    // Add to dropdown
+    $("#overlays").append($(`<option value="${file.name}">${file.name}</option>`));
+}
+
+const findExtrema = () => {
+    const datapoints = Object.values(uploads).reduce((datapoints, upload) => datapoints.concat(upload.points.map(point => point.data)), []);
+    const dataByType = datatypes.reduce((dataByTypeObj, datatype) => {
+        const dataByTypeArr = datapoints.reduce((dataByTypeArr, datapoint) => {
+            if (!isNaN(datapoint[datatype])) dataByTypeArr.push(datapoint[datatype]);
+            return dataByTypeArr;
+        }, []);
+        dataByTypeObj[datatype] = dataByTypeArr;
+        return dataByTypeObj;
+    }, {});
+    return datatypes.reduce((extremaObj, datatype) => {
+        extremaObj[datatype] = [Math.min(...dataByType[datatype]), Math.max(...dataByType[datatype])];
+        return extremaObj;
+    }, {});
+};
+
+const resetColors = datatype => {
+    Object.values(uploads).reduce((pointArr, upload) => pointArr.concat(upload.points), [])
+        .forEach(point => {
+            const pointColor = !isNaN(point.data[datatype])
+                ? rainbow(normalize(parseFloat(point.data[datatype]), ...extrema[datatype]))
+                : "gray";
+            point.marker.setStyle({ color: pointColor });
+    });
+};
+
+const resetDesc = () => {
+    Object.values(uploads).reduce((pointArr, upload) => pointArr.concat(upload.points), [])
+        .forEach(point => {
+            const description = datatypes.reduce((descStr, datatype) => descStr += datatype + ": " + (point.data[datatype] || "N/A") + "<br>", "");
+            point.marker.setPopupContent(description);
+    });
+};
+
+const resetRadio = displayedData => {
     $("#sensors").empty();
     
     datatypes.forEach(datatype => {
-        $("#sensors").append($(`<input type="radio" id="${clean(datatype)}" name="sensor" value="${datatype}">`))
+        $("#sensors").append($(`<input type="radio" name="sensor" class="sensor-radio-btn" value="${datatype}">`))
             .append(`<label for="${datatype}">${datatype}</label></div>`)
             .append(`<br>`);
     });
 
-    $(`#${clean(displayedData)}`).prop("checked", true);
-
-    $("input[type=radio][name=sensor]").change(() => {
-        const datatype = $('input[name="sensor"]:checked').val();
-        resetColors(datatype);
+    const radioButtons = $(".sensor-radio-btn");
+    const radioToCheck = radioButtons.filter(index => {
+        return radioButtons[index].value === displayedData
     });
-}
 
-const resetColors = datatype => {
-    points.forEach(pointData => {
-        if (!isNaN(pointData[1][datatype])) {
-            const pointColor = rainbow(normalize(parseFloat(pointData[1][datatype]), ...range[datatype]));
-            pointData[0].setStyle({ color: pointColor });
-        } else {
-            pointData[0].setStyle({ color: "gray" });
-        }
+    $(".sensor-radio-btn").click(() => {
+        const selectedDatatype = getDisplayedData();
+        resetColors(selectedDatatype);
     });
-}
+
+    if (radioToCheck.length === 1) {
+        radioToCheck[0].checked = true;
+        radioToCheck[0].click();
+    } else if (radioButtons.length > 0 && radioToCheck.length === 0) {
+        radioButtons[0].checked = true;
+        radioButtons[0].click();
+    } else {
+        console.log("No radio buttons generated");
+    }
+};
+
+const getDisplayedData = () => $("#sensors").children().length > 0 ? $('input[name="sensor"]:checked').val() : datatypes[0];
 
 const normalize = (value, min, max) => (value - min) / (max - min);
-
-const clean = string => [" ", "(", ")"].reduce((prevStr, char) => prevStr.replace(char, ""), string);
